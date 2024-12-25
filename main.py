1f2df814f788533fa33a9eb471677f56e423d648
Ayodeji Giwa <ayodeji.giwa@binghamuni.edu.ng>
2024-12-26 00:22:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ def base_converter():
    print("2. Octal")
    print("3. Decimal")
    print("4. Hexadecimal")
+   
+   try:
+       current_base = int(input("Enter your choice (1-4): "))
+        if current_base not in {1, 2, 3, 4}:
+           print("Invalid input! Please enter a number between 1 and 4.")
+           return
+    except ValueError:
+        print("Invalid input! Please enter a valid number between 1 and 4.")
+        return
+
   
 base_converter()
