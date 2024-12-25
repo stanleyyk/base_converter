363c2d70a471033c36c129b095699277f73bc8ab
stanleyyk <123550323+stanleyyk@users.noreply.github.com>
2024-12-26 00:16:49 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def base_converter():
+   print("Welcome to the Base Converter")
+   number = input("Enter the number: ")
+   print("Select the current base of the number:")
+   print("1. Binary")
+   print("2. Octal")
+   print("3. Decimal")
+   print("4. Hexadecimal")
+  
+base_converter()
